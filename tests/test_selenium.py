1a4715459f4e6a09a6fd61611d453f5f039e0471
Fernando <fwfurtado@gmail.com>
2021-05-01 08:54:33 -0300

    add selenium test

---
+++ tests/test_selenium.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_test():
+    with webdriver.Firefox() as driver:
+        driver.get("https://google.com/")
+
+        search_field = driver.find_element_by_name("q")
+        search_field.clear()
+        search_field.send_keys("naruto")
+        search_field.send_keys(Keys.RETURN)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.url_changes)
+        page = BeautifulSoup(driver.page_source)
+
+        links = page.find_all(name="a")
+
+        link = links[0]
+
+        print(link)
